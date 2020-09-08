c75f8b111536ee126e736b1008dfcbc2489dc2d4
Minh Duc Pham <minhduc99@outlook.com>
2020-09-07 23:59:20 -0400

    Deploy to GitHub Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TypingAnimationModule } from 'angular-typing-animation';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    TypingAnimationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
